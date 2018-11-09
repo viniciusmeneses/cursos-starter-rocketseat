d98c3ce0d1a5e111a1e9350f04b044c13cee1f84
vini23hot <vini23hot@gmail.com>
2018-11-08 23:23:09 -0200

    Implemented header of react app

--- starter-react/huntweb/src/App.js
+++ starter-react/huntweb/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 
+import Header from './components/Header'
+
+import './styles.css'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World!</h1>
+        <Header />
       </div>
     );
   }
